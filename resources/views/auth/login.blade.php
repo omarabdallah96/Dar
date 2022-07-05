2207fd6084d7503f418ec2dc03cb585a99616c00
omar <omarlb512@gmail.com>
2022-07-05 16:18:29 +0300

    edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('title')
+تسجيل الدخول
+
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('title')
+الرئيسية
+
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
